ff654a50d338da2623222171a0521997936b4ce0
EC2 Default User <ec2-user@ip-172-31-60-59.us-west-2.compute.internal>
2020-02-18 21:57:17 +0000

    Forgot .keys for rating params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,11 +14,12 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.ratings
     @sort = params[:sort] || session[:sort]
     @rating_param = params[:ratings] || session[:ratings] || @all_ratings
-    #@rating_param = params[:ratings].keys || session[:ratings]
+
+    @movies = Movie.where(rating: @rating_param.keys).order(@sort)
     #save sessions
     session[:sort] = @sort
     session[:ratings] = @rating_param
-    @movies = Movie.where(rating: @rating_param).order(@sort)
+   
     if (params[:sort] != session[:sort]) or (params[:ratings] != session[:ratings])
       #flash.keep
       #redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
