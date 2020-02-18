5564793b4f922cb21b8c85debd9a7126ebaba0d2
EC2 Default User <ec2-user@ip-172-31-60-59.us-west-2.compute.internal>
2020-02-18 21:04:07 +0000

    Adding sessions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,10 @@ class MoviesController < ApplicationController
     session[:sort] = @sort
     session[:ratings] = @rating_param
     @movies = Movie.where(rating: @rating_param).order(@sort)
+    if (params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:ratings].nil?))
+      flash.keep
+      redirect_to movie_path(sort: session[:sort], ratings: session[:ratings])
+    end
   end
 
   def new
