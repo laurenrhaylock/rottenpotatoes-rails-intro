19d03b3eaab6b5881c08c34ca421f36eb0a5c266
EC2 Default User <ec2-user@ip-172-31-60-59.us-west-2.compute.internal>
2020-02-18 22:39:26 +0000

    New approach

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,21 +11,32 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.ratings
+    @all_ratings = Movie.all_ratings
     @sort = params[:sort] || session[:sort]
+    @ratings = params[:ratings]  || session[:ratings] || all_ratings
+    @movies = Movie.where( { rating: @ratings.keys } ).order(@sort)
+    session[:sort], session[:ratings] = @sort, @ratings
+    
+    if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+      flash.keep
+      redirect_to movies_path sort: @sort, ratings: @ratings
+    end
+    
+   # @all_ratings = Movie.ratings
+   # @sort = params[:sort] || session[:sort]
     #session[:ratings] = session[:ratings] || @all_ratings
     #params[:ratings].nil? ? @rating_param = session[:ratings] : @rating_param = params[:ratings].keys
-    @rating_param = params[:ratings] || session[:ratings] || all_ratings
+   # @rating_param = params[:ratings] || session[:ratings] || all_ratings
     #save sessions
-    session[:sort] = @sort
-    session[:ratings] = @rating_param
-    @movies = Movie.where(rating: @rating_param.keys ).order(@sort)
-    if (params[:sort] != session[:sort]) or (params[:ratings] != session[:ratings])
-      flash.keep
-      params[:sort] = session[:sort]
-      params[:ratings] = session[:ratings]
-      redirect_to movies_path(sort: @sort, rating: @rating_param)
-    end
+   # session[:sort] = @sort
+   # session[:ratings] = @rating_param
+   # @movies = Movie.where(rating: @rating_param.keys ).order(@sort)
+   # if (params[:sort] != session[:sort]) or (params[:ratings] != session[:ratings])
+    #  flash.keep
+    #  params[:sort] = session[:sort]
+    #  params[:ratings] = session[:ratings]
+    #  redirect_to movies_path(sort: @sort, rating: @rating_param)
+   # end
   end
 
   def new
